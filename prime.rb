0602f1273850ae2a7c8ccf3cab51d8e923508b45
Ariela Cohen <arielaades@gmail.com>
2019-07-29 19:34:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 def prime?(integer)
-  case integer
-  when 1..3
+  case 
+  when integer.between?(1, 3)
     return true
   when integer < 0
     return false
   else
   (2..10).each do |n|
-if (integer % n) == 0
-  return false
-end
-end
-end
+    if (integer % n) == 0
+      return false
+    end
+  end
+  end
 return true
 end
