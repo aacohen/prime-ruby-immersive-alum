fc12fa3de186f20ba52df10916b7a9b0cf2f5809
Ariela Cohen <arielaades@gmail.com>
2019-07-29 19:28:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,14 @@
 def prime?(integer)
-  (2..integer).each do |n|
+  case integer
+  when 1..2
+    return true
+  when <1
+    return false
+  else
+  (1..integer).each do |n|
 if (integer % n) == 0
   return false
+end
 end
 end
 return true
